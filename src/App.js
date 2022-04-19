47fc5aa9e973698d8a8a4c3c4c5103482e852729
alvaro-ffarfan <floresfarfanalvaro@gmail.com>
2022-04-19 12:11:31 -0400

    creating all types of buttons examples

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-
+import React from "react";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div>
-      <Button>
-        este es un Boton
+      <Button color="primary">color</Button>
+      <Button color="primary" variant="contained">
+        color
+      </Button>
+      <Button color="primary" variant="contained" disableElevation>
+        color
+      </Button>
+      <Button color="primary" variant="contained" href="https://google.com">
+        google
+      </Button>
+      <Button color="primary" variant="contained" fullWidth>
+        fullWidth
+      </Button>
+      <Button color="primary" variant="contained" size="small">
+        fullWidth
+      </Button>
+      <Button color="primary" variant="contained" size="large">
+        fullWidth
+      </Button>
+      <Button color="primary" variant="contained" size="medium">
+        fullWidth
       </Button>
     </div>
   );
